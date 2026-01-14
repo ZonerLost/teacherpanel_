c1230c150aa206263a529efe48fcff9cf665f709
Rashid Khan <rashidiyaoo7@gmail.com>
2026-01-14 10:11:34 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Example for a typical src setup
+    "./public/*.html", // Example for public HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
